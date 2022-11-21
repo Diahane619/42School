ad427c39f0066593af791d9377caca6be4774dae
Diahane619 <f.vitali89@gmail.com>
2022-11-21 15:07:43 +0100

    rtj

---
+++ GET_NEXT_LINE/Get_mia/get_next_line.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: francevi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/21 15:06:50 by francevi          #+#    #+#             */
+/*   Updated: 2022/11/21 15:07:38 by francevi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+
+# ifndef BUFFER_SIZE
+
+#  define BUFFER_SIZE 42
+
+# endif
+
+char	*get_next_line(int fd);
+size_t	gnl_strlen(const char *s);
+int		gnl_strchr_i(const char *s, int c);
+size_t	gnl_strlcpy(char *dst, const char *src, size_t size);
+char	*gnl_substr(char const *s, unsigned int start, size_t len);
+size_t	gnl_strlcat(char *dst, const char *src, size_t size);
+char	*gnl_shrink_buffer(char *buf, char *line);
+char	*gnl_expand_buffer(char *buf, int fd);
+char	*gnl_newread(int fd);
+
+#endif
