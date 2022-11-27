eb216e7aa45133d14cca0a3126609c5bd9d53212
Diahane619 <f.vitali89@gmail.com>
2022-11-27 19:18:33 +0100

    rh

--- diahane.c
+++ diahane.c
@@ -3,13 +3,10 @@
 int	 main()
 {
 	int	a;
-	int	b;
-	int c;
 
-	printf("Valore di a: ");
 	scanf("%d", &a);
-	printf("Valore di b: ");
-	scanf("%d", &b);
-	c = a + b;
-	printf("Somma: %d\n", c);
+	if (a%2 == 0)
+	printf("Pari\n");
+	else
+	printf("Dispari\n");
 }
