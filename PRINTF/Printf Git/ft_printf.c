19920a78f24a8f7a88f7abc982761f04030b93b7
Diahane619 <francevi@student.42firenze.it>
2022-12-15 11:42:12 +0100

    f

--- PRINTF/Printf Git/ft_printf.c
+++ PRINTF/Printf Git/ft_printf.c
@@ -39,23 +39,23 @@ int ft_printf(const char *format, ...) // Il format rappresenta tutto il contenu
 {
 	int	i; // contatore di format
 	int	len; // lunghezza totale di format
-	int	res; // il risultato finale del printf (il format dopo le tutte le conversioni dei formati) 
+	int	count; // il risultato finale del printf (il format dopo le tutte le conversioni dei formati) 
 	va_list args; // dichiarazione del numero di argomenti per la funzione variadica 
 
 	va_start (args, format); // faccio partire la funzione variadica passondogli gli argomenti (che varieranno) e il contenuto (format) 
 	i = 0; //inizializzo il contatore
-	res = 0;
+	count = 0;
 	len = ft_strlen(format);
 	while (format[i] && i < len) // fintanto che il contenuto esiste e il contatore i non raggiunge la fine
 	{
 		if (format[i] != '%') // se non incontra il %
-			ft_putchar(format[i++], &res); // stampa e vammi avanti
+			ft_putchar(format[i++], &count); // stampa e vammi avanti
 		else
 		{	
 			i += 1; // vado al carattere dopo il %
-			ft_placeholder(args, format, &res, &i); // controlla il tipo di placeholder(%cdiusxXp) e lo rimpiazza con il valore degli args 
+			ft_placeholder(args, format, &count, &i); // controlla il tipo di placeholder(%cdiusxXp) e lo rimpiazza con il valore degli args 
 		}
 	}
 	va_end (args); // chiude la variadica
-	return (res); // ritorna il risultato finale
+	return (count); // ritorna il risultato finale
 }
