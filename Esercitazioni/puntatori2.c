9cbee0383ba0f0dbca550b52a713a79231188419
Diahane619 <francevi@student.42firenze.it>
2023-01-24 13:49:49 +0100

    fjht

--- Esercitazioni/puntatori2.c
+++ Esercitazioni/puntatori2.c
@@ -1,17 +1,31 @@
-void bubblesort(int v[], int n) 
-{
-	int temp;
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-for(int i = 0; i < n - 1; i++) 
+void main()
 {
- for(int k = 0; k < n - 1 - i; k++) 
- {
-         if(v[k] > v[k + 1]) 
-		 {
-          	temp = v[k];
-          	v[k] = v[k + 1];
-          	v[k + 1] = temp;
-         }
- }
-}
+	char *stringa = malloc(sizeof(stringa)); 
+	short int palindrome = 1;		//1 = stringa palindrome; 2 = stringa non palindrome
+	
+	printf("Inserire una stringa: ");
+	scanf("%s", stringa);     			//legge la stringa da tastiera
+	int i = 0;
+	int	j = strlen(stringa) - 1;
+	/*ciclo di controllo: i parte dalla prima lettera e viene incrementata fino alla metà
+	della lunghezza della stringa, j parte dall'ultima lettera e viene decrementata
+	fino alla metà della lunghezza della stringa*/
+	while (i < (j +  1)/2)
+	{
+		if(stringa[i] != stringa[j]) //se trova due lettere diverse...
+		{
+			palindrome = 0;  //assegna 0 a "palindrome"...
+			break;          //ed esce dal ciclo
+		}
+		i++;
+		j--;
+	}
+	if (palindrome) 
+		printf("La stringa è palindrome\n");
+	else 
+		printf("La stringa non è palindrome\n");
 }
