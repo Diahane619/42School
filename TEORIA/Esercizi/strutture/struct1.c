7b6ed2ad09d2fb0a3630f8b5ff0b9d9c9f4c0893
Diahane619 <francevi@student.42firenze.it>
2023-03-13 23:10:57 +0100

    d fl

--- TEORIA/Esercizi/strutture/struct1.c
+++ TEORIA/Esercizi/strutture/struct1.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <math.h>
 
 struct struct_punti
 {
@@ -18,43 +19,15 @@ int main()
 		scanf("%d%d", &punti[i].x, &punti[i].y);
 		i++;
 	}
-	
 	int j = 0;
-	int xmax;
-	int xmin;
-	int	ymin;
-	int ymax;
-	int m;
-	int n;
-	int h;
-	while(j < 4)
-	{
-		if (punti[j].x >= punti[j + 1].x)
-			xmax = punti[j].x;
-		m = 0;
-		if (punti[m].x <= punti[m + 1].x)
-			xmin = punti[m].x;
-		n = 0;
-		if (punti[n].y <= punti[n + 1].y)
-			ymin = punti[m].y;
-		h = 0;
-		if (punti[h].y >= punti[h + 1].y)
-			ymax = punti[j].y;
-		j++;
-		m++;
-		n++;
-		h++;
+	int	perimetro = 0;
+	i = 0;
+    while (i < 4)
+    {
+        j = (i + 1) % 4;
+        perimetro += sqrt( (punti[i].x-punti[j].x)*(punti[i].x-punti[j].x) +
+                           (punti[i].y-punti[j].y)*(punti[i].y-punti[j].y) );
+		i++;
 	}
-	while(++i < 4)
-	{
-		if(punti[i].x = xmax)
-			j = punti[i].x;
-		if(punti[i].x = xmin)
-			m = punti[i].x;
-		if(punti[i].x = ymin)
-			n = punti[i].x;
-		if(punti[i].x = ymax)
-			h = punti[i].x;
-	}
-
+    printf("Lunghezza perimetro: %d\n", perimetro);
 }
