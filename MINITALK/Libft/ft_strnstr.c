da00d68d3125c3556fc921ef2f9d3ed3fbc0634c
Diahane619 <francevi@student.42firenze.it>
2023-04-01 13:21:36 +0200

    df

---
+++ MINITALK/Libft/ft_strnstr.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: francevi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/31 15:30:37 by francevi          #+#    #+#             */
+/*   Updated: 2022/10/31 16:16:28 by francevi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *big, const char *little, size_t len)
+{
+	size_t	n;
+
+	if (*little == 0)
+		return ((char *)big);
+	n = ft_strlen(little);
+	while (*big && n <= len)
+	{
+		if (*big == *little && ft_strncmp(big, little, n) == 0)
+			return ((char *)big);
+		++big;
+		--len;
+	}
+	return (NULL);
+}
