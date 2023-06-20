6c13fe5618ff81e6322af184600f0fed1503d01a
Diahane619 <f.vitali89@gmail.com>
2023-06-20 17:19:47 +0200

    ef

--- MINITALK/Minitalk_Kirill/project/src/minitalk_client.c
+++ MINITALK/Minitalk_Kirill/project/src/minitalk_client.c
@@ -2,7 +2,13 @@
 
 volatile char *g_bits_to_send = 0; // Dichiarazione di una variabile globale char *g_bits_to_send
 
-// La funzione encode_bits prende un carattere come parametro e lo codifica in 8 bit. Questi bit vengono poi aggiunti ad un array char globale, g_bits_to_send. Se il bit è 1, viene aggiunto '1' all'array, altrimenti viene aggiunto '0'. La funzione utilizza un ciclo while che itera 8 volte per codificare ogni bit del carattere. All'interno del ciclo, viene utilizzato l'operatore AND bit a bit per verificare se il bit corrente del carattere è 1. Se è 1, viene aggiunto '1' all'array g_bits_to_send utilizzando la funzione ft_char_append, altrimenti viene aggiunto '0'.
+/* La funzione encode_bits prende un carattere come parametro e lo codifica in 8 bit.
+Questi bit vengono poi aggiunti ad un array char globale, g_bits_to_send. 
+Se il bit è 1, viene aggiunto '1' all'array, altrimenti viene aggiunto '0'. 
+La funzione utilizza un ciclo while che itera 8 volte per codificare ogni bit del carattere. 
+All'interno del ciclo, viene utilizzato l'operatore AND bit a bit per verificare se il bit corrente
+del carattere è 1. Se è 1, viene aggiunto '1' all'array g_bits_to_send utilizzando 
+la funzione ft_char_append, altrimenti viene aggiunto '0'. */
 static void encode_bits(char c)
 {
 	size_t i; // Dichiarazione di una variabile di tipo size_t
@@ -19,7 +25,18 @@ static void encode_bits(char c)
 
 /* La funzione send_message prende l'ID di un processo server e una stringa di messaggio come parametri.
 La funzione codifica ogni carattere del messaggio in bit utilizzando la funzione encode_bits e invia i
- bit al processo server utilizzando i segnali SIGUSR1 e SIGUSR2. Ogni bit viene inviato come segnale, con un ritardo di 50 microsecondi tra ogni segnale. La funzione utilizza un ciclo while che itera per la lunghezza della stringa di messaggio per codificare ogni carattere del messaggio in bit. All'interno del ciclo, viene chiamata la funzione encode_bits con il carattere corrente della stringa di messaggio come parametro. Viene quindi utilizzato un altro ciclo while che itera 8 volte per inviare ogni bit del carattere codificato al processo server. All'interno di questo ciclo, viene verificato se il bit corrente è uguale a '1'. Se è uguale a '1', viene assegnato il segnale SIGUSR1 alla variabile sig, altrimenti viene assegnato il segnale SIGUSR2. Viene quindi inviato il segnale sig al processo server con ID server_pid utilizzando la funzione kill. Viene quindi eseguito un delay di 50 microsecondi utilizzando la funzione usleep. Alla fine del ciclo interno, viene liberata la memoria allocata per la variabile g_bits_to_send utilizzando la funzione ft_free.
+bit al processo server utilizzando i segnali SIGUSR1 e SIGUSR2. Ogni bit viene inviato come segnale,
+con un ritardo di 50 microsecondi tra ogni segnale. La funzione utilizza un ciclo while che itera per
+la lunghezza della stringa di messaggio per codificare ogni carattere del messaggio in bit.
+All'interno del ciclo, viene chiamata la funzione encode_bits con il carattere corrente della
+stringa di messaggio come parametro. Viene quindi utilizzato un altro ciclo while che itera 8
+volte per inviare ogni bit del carattere codificato al processo server. 
+All'interno di questo ciclo, viene verificato se il bit corrente è uguale a '1'. Se è uguale a '1', viene
+assegnato il segnale SIGUSR1 alla variabile sig, altrimenti viene assegnato il segnale SIGUSR2. 
+Viene quindi inviato il segnale sig al processo server con ID server_pid utilizzando la funzione kill. 
+Viene quindi eseguito un delay di 50 microsecondi utilizzando la funzione usleep. 
+Alla fine del ciclo interno, viene liberata la memoria allocata per la variabile g_bits_to_send utilizzando 
+la funzione ft_free.*/
 static void send_message(pid_t server_pid, char *message)
 {
 	int sig; // Dichiarazione di una variabile di tipo int
