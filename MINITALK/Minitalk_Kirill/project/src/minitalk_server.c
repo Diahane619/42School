f2d80467508246a8b71a6b560351873c5ea533f5
Diahane619 <115653185+Diahane619@users.noreply.github.com>
2023-06-20 15:06:55 +0000

    dn

--- MINITALK/Minitalk_Kirill/project/src/minitalk_server.c
+++ MINITALK/Minitalk_Kirill/project/src/minitalk_server.c
@@ -14,17 +14,7 @@
 
 volatile char	*g_received_bits = 0; // definisce una variabile globale volatile di tipo char puntatore, inizializzata a 0
 
-/**
- * @brief 
-	Decodifica 8 bit in un carattere.
- * @details 
-	La funzione prende un array globale di char contenente 8 bit e decodifica i 
-	bit in un carattere utilizzando operatori bitwise.
- * @param bits 
-	Gli 8 bit da decodificare.
- * @return 
-	Il carattere decodificato.
- */
+// La funzione decode_bits prende un array di char contenente 8 bit come parametro e decodifica i bit in un carattere utilizzando operatori bitwise. La funzione utilizza un ciclo while che itera 8 volte per decodificare ogni bit del carattere. All'interno del ciclo, viene verificato se il bit corrente è uguale a '1'. Se è uguale a '1', viene eseguito un OR bitwise tra la variabile c e il risultato dello shift a sinistra di 1 di i posizioni. Altrimenti, viene eseguito un OR bitwise tra la variabile c e 0. Alla fine del ciclo, viene restituito il valore della variabile c.
 static char	decode_bits(volatile char *bits)
 {
 	char	c = 0; // inizializza la variabile c a 0
@@ -40,17 +30,7 @@ static char	decode_bits(volatile char *bits)
 	return (c); // restituisce il valore di c
 }
 
-/**
- * @brief 
-	Gestisce gli eventi di segnale inviati al processo
- * @details 
-	Quando viene ricevuto un segnale SIGUSR1 o SIGUSR2, questa funzione aggiunge un 
-	bit corrispondente alla variabile globale g_received_bits; 
-	Se la sequenza di bit raggiunge 8 caratteri, la sequenza viene decodificata in un 
-	carattere e stampata sull'output standard.
- * @param sig 
-	Il numero del segnale ricevuto.
- */
+// La funzione sig_handler gestisce gli eventi di segnale inviati al processo. Quando viene ricevuto un segnale SIGUSR1 o SIGUSR2, questa funzione aggiunge un bit corrispondente alla variabile globale g_received_bits. Se la sequenza di bit raggiunge 8 caratteri, la sequenza viene decodificata in un carattere utilizzando la funzione decode_bits e stampata sull'output standard utilizzando la funzione write.
 static void	sig_handler(int sig)
 {
 	char	c;
@@ -69,22 +49,7 @@ static void	sig_handler(int sig)
 	}
 }
 
-/**
- * @brief 
-	Funzione principale per il processo server.
- * @details 
-	La funzione inizializza un gestore di segnali per SIGUSR1 e SIGUSR2 e attende 
-	segnali in arrivo; 
-	Una volta ricevuto un segnale, viene chiamata la funzione gestore del segnale e il 
-	bit corrispondente viene aggiunto a un array globale di char; 
-	Una volta ricevuti 8 bit, vengono decodificati in un carattere e stampati sull'output standard utilizzando la funzione write; 
- * @param argc 
-	Il numero di argomenti passati alla funzione.
- * @param argv 
-	Gli argomenti passati alla funzione.
- * @return 
-	Il codice di uscita della funzione.
- */
+// La funzione main è la funzione principale per il processo server. La funzione inizializza un gestore di segnali per SIGUSR1 e SIGUSR2 e attende segnali in arrivo. Una volta ricevuto un segnale, viene chiamata la funzione gestore del segnale e il bit corrispondente viene aggiunto a un array globale di char. Una volta ricevuti 8 bit, vengono decodificati in un carattere e stampati sull'output standard utilizzando la funzione write.
 int	main(int argc, char **argv)
 {
 	struct sigaction	act;
