c28539f15ac55bc37b5c96d0c2e202d689c1d787
Diahane619 <f.vitali89@gmail.com>
2023-06-21 09:13:40 +0200

    sw

--- 42_EXAM/success/expand_str/expand_str.c
+++ 42_EXAM/success/expand_str/expand_str.c
@@ -1,42 +1,27 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   expand_str.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: francevi <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/06/05 11:19:00 by francevi          #+#    #+#             */
-/*   Updated: 2023/06/05 12:07:58 by francevi         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <unistd.h>
 
-int main(int argc, char **argv)
+int main(int ac, char **av)
 {
-    int i = 0;
-    int len = 0;
-    int first_word = 1;
+	int i = 0;
+	int flag;
 
-    if (argc == 2)
-    {
-        while (argv[1][i] == ' ' || argv[1][i] == '\t')
-            ++i;
-        while (argv[1][i])
-        {
-            while (argv[1][i] && argv[1][i] != ' ' && argv[1][i] != '\t')
-            {
-                ++len;
-                ++i;
-            }
-            if (len > 0 && first_word == 0)
-                write(1, "   ", 3);
-            first_word = 0;
-            write(1, &argv[1][i - len], len);
-            len = 0;
-            while (argv[1][i] == ' ' || argv[1][i] == '\t')
-                ++i;
-        }
-    }
-    write(1, "\n", 1);
+	if (ac == 2)
+	{
+		while(av[1][i] == ' ' || av[1][i] == '\t')
+			i++;
+		while(av[1][i])
+		{
+			if(av[1][i] == ' ' || av[1][i] == '\t')
+				flag = 1;
+			if(!(av[1][i] == ' ' || av[1][i] == '\t'))
+			{
+				if(flag)
+					write(1, "   ", 3);
+				flag = 0;
+				write(1, &av[1][i], 1);
+			}
+			i++;
+		}
+	}
+	write(1, "\n", 1);
 }
