2b6baf5df6000a3e4ae844e8652e5f8c9a5b9d9a
Diahane619 <115653185+Diahane619@users.noreply.github.com>
2024-09-22 18:43:16 +0000

    uihbi

--- C#/obj/Debug/net8.0/C#.AssemblyInfo.cs
+++ C#/obj/Debug/net8.0/C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+164246333d4fdce7c69afb00bb4aa239f315ae0c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+765b80076d144f36109743d7707e60e11ebacf34")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
